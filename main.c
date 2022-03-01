32c5a2bd2a169a3245afe01099e916766e8ca954
Jindřich Vodák <jindrich.vodak@email.cz>
2022-03-01 19:22:22 +0100

    cpu_usage update

--- main.c
+++ main.c
@@ -4,12 +4,28 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
+#define PROC_LINE_LENGTH 64
+
 int cpu_usage()
 {
+    unsigned long long int usertime, nicetime, systemtime, idletime;
+    unsigned long long int ioWait, irq, softIrq, steal, guest, guestnice;
+    char buffer[PROC_LINE_LENGTH + 1];
+
+    FILE *test = popen("cat /proc/stat", "r");
+    fgets(buffer, PROC_LINE_LENGTH, test);
+    sscanf(buffer, "cpu  %16llu %16llu %16llu %16llu %16llu %16llu %16llu %16llu %16llu %16llu",
+           &usertime, &nicetime, &systemtime, &idletime, &ioWait, &irq, &softIrq, &steal, &guest, &guestnice);
+    fprintf(stderr, "%llu\n", nicetime);
+    fprintf(stderr, "TEST_CPU\n");
+
     return 0;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
+    cpu_usage();
+    fprintf(stderr, "TEST_MAIN\n");
+
     return 0;
 }
